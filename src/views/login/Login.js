f9ed7048f3df7b898f0f38218bf5f3a90dcbfd55
godlovericea <1020097250@qq.com>
2020-06-05 21:17:45 +0800

    mapbox

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,77 +1,43 @@
-import React,{useState} from 'react'
-import { Form, Input, Button, Checkbox } from 'antd';
-
-const formlayout = {
-  labelCol: {
-    span: 8,
-  },
-  wrapperCol: {
-    span: 10,
-  },
-};
-const tailLayout = {
-  labelCol: {
-    span: 8,
-  },
-  wrapperCol: {
-    offset: 8,
-    span: 10,
-  },
-};
+import React, { useState, useEffect, useRef } from "react"
+import {Box} from '@material-ui/core'
+import mapboxgl from 'mapbox-gl'
+import './login.css'
+import allData from '@/assets/jsonData/all.json'
 
 const Login = () => {
-  const onFinish = values => {
-    console.log('Success:', values);
-  };
-
-  const onFinishFailed = errorInfo => {
-    console.log('Failed:', errorInfo);
-  };
-  const [name,setName] = useState('')
-  const [pwd,setPwd] = useState('')
-  return (
-    <div>
-      <Form {...formlayout} name="basic" initialValues={{ remember: true}} onFinish={onFinish} onFinishFailed={onFinishFailed}>
-        <Form.Item label="Username" name="username" 
-          rules={[
-            {
-              required: true,
-              message: 'Please input your username!',
-            },
-          ]}
-        >
-          <Input maxLength={10} onChange={(event)=>{
-            setName(event.target.value)
-          }}/>
-        </Form.Item>
-
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your password!',
-            },
-          ]}
-        >
-          <Input.Password maxLength={20} onChange={(event)=>{
-            setPwd(event.target.value)
-          }}/>
-        </Form.Item>
-
-        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <Form.Item {...tailLayout}>
-          <Button type="primary" htmlType="submit" onClick={}>
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
-  );
-};
+    const mapRef = useRef(null)
+    const mapCanvas = useRef(null)
+    useEffect(()=>{
+        mapboxgl.accessToken = 'pk.eyJ1Ijoibnl5anl5YW5mYXBlbmciLCJhIjoiY2p3ajU4eXI2MGdxcDQ4cGI4cHI2bHhjcSJ9.m4FzyOH_5Yo3YVnroLxk-w';
+        mapRef.current = new mapboxgl.Map({
+            container: mapCanvas.current,
+            style: "mapbox://styles/zplinze/ck2wvh2pa05ip1cr8supcaivg",
+            center: [118.723, 32.011],
+            zoom: 12.0
+        });
+        const map = mapRef.current
+        map.on("load", () => {
+            map.addSource('earthquakes',{
+              type:'geojson',
+              data:allData
+            })
+            map.addLayer({
+              id: "earthquake_circle",
+              type: "circle",
+              source: "earthquakes",
+              paint: {
+                  "circle-opacity": 0.6,
+                  "circle-radius": 20,
+              },
+          })
+        })
+    }, [])
+    return <Box component="div" className="mapcontainer">
+      <Box component="div" style={{width:'100%',height:'100%'}}>
+        <Box component="div" ref={mapCanvas} style={{width:'100%',height:'100%'}}>
+        </Box>
+      </Box>
+    </Box>
+}
 
 export default Login
